85596d18d527e9b5a76ee5a5126d184cc819eb95
Theodoros Chaikalis <theodoros.chaikalis@intrasoft-intl.com>
2020-11-23 19:08:15 +0200

    fix test

--- src/test/java/com/example/MyResourceTest.java
+++ src/test/java/com/example/MyResourceTest.java
@@ -22,7 +22,7 @@ public class MyResourceTest extends JerseyTest {
      */
     @Test
     public void testGetIt() {
-        final String responseMsg = target().path("myresource").request().get(String.class);
+        final String responseMsg = target().path("test").request().get(String.class);
 
         assertEquals("Hello, Heroku!", responseMsg);
     }
